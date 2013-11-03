ce178621224ad78257369a532cc2c9083e87ca50
jameselsey <james.elsey@gmail.com>
2013-11-03 18:58:44 +0000

    Added shortening (need some improvements such as randomising characters)

--- routes/shorty.js
+++ routes/shorty.js
@@ -9,7 +9,12 @@ exports.createShort = function (req, res) {
         res.render('short', {message: 'Request did not contain a url to shorten, please provide urlToShorten'})
     } else {
         console.log("Shortening url of " + urlToShorten);
-        res.render('short', {shortUrl: "ShRtU123"});
+        var baseUrl = 'http://localhost:' + req.app.get('port') + '/'
+        var shortUrlCode = createShortUrl(urlToShorten)
+
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+        res.render('short', { shortUrl: baseUrl + shortUrlCode });
     }
 };
 
@@ -20,7 +25,40 @@ exports.getLong = function (req, res) {
     // grab the path and strip the leading slash
     var shortUrl = req.path.substring(1)
 
-    console.log("fetching short url " + shortUrl)
+    console.log("fetching short url " + shortUrlCode)
 
-    res.send("This will return the long url from " + shortUrl);
+    res.send("This will return the long url from " + shortUrlCode);
 };
+
+var id = 0;
+var longToShort = new Array();
+var shortToLong = new Array();
+var CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHUJKLMNOPQRSTUVWXYZ';
+
+function numToBase62(n) {
+    if (n > 62) {
+        return numToBase62(Math.floor(n / 62)) + CHARS[n % 62];
+    } else {
+        return CHARS[n];
+    }
+}
+
+function createShortUrl(longUrl) {
+    console.log("Shortening url of " + longUrl);
+
+    /* Check whether the url has been added before */
+    shortUrlCode = longToShort[longUrl];
+    if (shortUrlCode == undefined) {
+        shortUrlCode = numToBase62(id);
+        while (shortUrlCode.length < 5) {
+            /* Add padding */
+            shortUrlCode = CHARS[0] + shortUrlCode;
+        }
+        longToShort[longUrl] = shortUrlCode;
+        shortToLong[shortUrlCode] = longUrl;
+        id++;
+    }
+
+    return shortUrlCode;
+}
+
