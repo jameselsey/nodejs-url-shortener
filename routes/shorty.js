8f4b2d07589b9260d331cbc6716e5cc0367111ab
jameselsey <james.elsey@gmail.com>
2013-12-07 13:48:35 +0000

    Short codes are now random alphanumeric strings.

--- routes/shorty.js
+++ routes/shorty.js
@@ -1,20 +1,24 @@
+// these hold the mappings between short codes and longUrls
+var longToShort = [];
+var shortToLong = [];
+
 /*
  * POST creates a short url from a long url
  */
 exports.createShort = function (req, res) {
-    var urlToShorten = req.body.urlToShorten;
 
+    var urlToShorten = req.body.urlToShorten;
     if (!urlToShorten) {
         console.log('Request did not contain a url to shorten, please provide urlToShorten');
         res.render('short', {message: 'Request did not contain a url to shorten, please provide urlToShorten'});
     } else {
+
         console.log("Request to shorten " + urlToShorten);
 
         urlToShorten = addhttp(urlToShorten);
-
         var baseUrl = 'http://localhost:' + req.app.get('port') + '/';
-        var shortCode = createShortCode(urlToShorten);
 
+        var shortCode = createShortCode(urlToShorten);
         res.setHeader('Content-Type', 'text/html');
         res.statusCode = 200;
         res.render('short', { shortUrl: baseUrl + shortCode });
@@ -25,32 +29,20 @@ exports.createShort = function (req, res) {
  * GET retrieves long url from short url
  */
 exports.getLong = function (req, res) {
+
     // grab the path and strip the leading slash
     var shortCode = req.path.substring(1);
 
     console.log("Fetching URL indexed by " + shortCode);
-
     var theLongUrl = shortToLong[shortCode];
+
     console.log('Short code ' + shortCode + " refers to " + theLongUrl);
 
     console.log("redirecting to " + theLongUrl);
-
     res.writeHead(302, {'Location': theLongUrl});
     res.end();
 };
 
-var id = 0;
-var longToShort = [];
-var shortToLong = [];
-var CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHUJKLMNOPQRSTUVWXYZ';
-
-function numToBase62(n) {
-    if (n > 62) {
-        return numToBase62(Math.floor(n / 62)) + CHARS[n % 62];
-    } else {
-        return CHARS[n];
-    }
-}
 
 function createShortCode(longUrl) {
     console.log("Creating short code for url " + longUrl);
@@ -60,20 +52,21 @@ function createShortCode(longUrl) {
 
     if (shortUrlCode == undefined) {
         console.log(longUrl + " has not already been shortened, so shortening it now.");
-        shortUrlCode = numToBase62(id);
-        while (shortUrlCode.length < 5) {
-            /* Add padding */
-            shortUrlCode = CHARS[0] + shortUrlCode;
-        }
-
+        shortUrlCode = randomString(5);
         console.log("Shortened " + longUrl + " to a shortcode of " + shortUrlCode);
 
         longToShort[longUrl] = shortUrlCode;
         shortToLong[shortUrlCode] = longUrl;
-        id++;
     }
 
     return shortUrlCode;
+}
+
+function randomString(length) {
+    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHUJKLMNOPQRSTUVWXYZ';
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
 }
 
 function addhttp(url) {
